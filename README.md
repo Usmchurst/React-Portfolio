f488156285d7aff803b815c617926e7c77afcab0
Eric Hurst <ericahurst77@gmail.com>
2022-05-28 23:47:27 -0400

    make it better

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # React-Portfolio
+
+Description
+
+This is a Personal react portfolio that is built with react, node, Javascript, CSS, and HTML. This portfolio 
+has all materials needed to showcase the projects I took part in developing in addition to the my resume. and contact me page. It is deployed on heroku to display functionality. 
+
+# Deployment link 
+
+
+
+# Repository link
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,4 +1,5 @@
 import Erics from '../../images/Erics.png';
+import Ericss from '../../images/Ericss.png';
 function Resume() { 
 // export default function Resume() {
   return (
@@ -6,7 +7,7 @@ function Resume() {
       <h1 className="nav nav-item justify-content-center" >Resume</h1>
       <div>
         <img alt="Eric's Resume" src={Erics} /> 
-
+        <img alt="Eric's Resume" src={Ericss} />
       </div>
         
       
